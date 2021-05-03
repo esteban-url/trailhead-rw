89eaf57f9fb8ba51507099c1500632cd7bca6682
Esteban Rojas <estebanjrojas@gmail.com>
2021-05-02 18:21:58 -0600

    add lorem to about page

--- web/src/pages/PrivatePage/PrivatePage.js
+++ web/src/pages/PrivatePage/PrivatePage.js
@@ -1,4 +1,3 @@
-import Header from 'src/components/Header/Header'
 import { PageTitle } from 'src/utils/PageTitle'
 
 const PrivatePage = () => {
@@ -7,6 +6,25 @@ const PrivatePage = () => {
       <PageTitle>Private Page</PageTitle>
 
       <p>This page is for logged in trailhead users only</p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu tempus
+        leo. Donec at est vehicula, cursus nibh ac, dapibus augue. Sed massa
+        nunc, dictum elementum posuere in, vestibulum ac enim. Nullam leo massa,
+        tincidunt sed sem quis, convallis ultrices lorem. Nam ut lobortis diam,
+        nec iaculis ligula. Ut sodales, nulla et laoreet ultricies, ligula odio
+        varius est, ac ullamcorper sapien est et magna. Proin ornare orci sit
+        amet sem facilisis aliquet. In non mauris egestas velit pulvinar
+        eleifend sed eu diam. Donec non felis accumsan, lobortis purus ut,
+        hendrerit sapien. Morbi ornare feugiat justo, a facilisis libero.
+        Quisque felis eros, tempor non viverra eget, euismod eget erat.
+        Pellentesque consectetur est quis mi scelerisque blandit. Nullam
+        interdum hendrerit efficitur. Sed eget arcu iaculis, blandit risus non,
+        pharetra ligula. Cras enim lacus, tincidunt quis orci a, molestie dictum
+        neque. Ut ut tempus quam. Sed non libero suscipit, gravida tortor sed,
+        eleifend ipsum. Vestibulum pharetra ornare nisl vel sodales. Etiam
+        faucibus dolor iaculis hendrerit dictum.
+      </p>
     </>
   )
 }

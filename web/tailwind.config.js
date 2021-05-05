5ac38cd93bb9839213a4e3bc886615d51e562519
Esteban Rojas <estebanjrojas@gmail.com>
2021-05-04 19:11:48 -0600

    enable tailwind JIT

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {

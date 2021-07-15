d7d676c123d3da9b18602c60108403f23cf512fe
Esteban Rojas <estebanjrojas@gmail.com>
2021-07-15 17:11:31 -0600

    upgrade redwood to 0.33

--- web/src/pages/AdminUserViewPage/AdminUserViewPage.js
+++ web/src/pages/AdminUserViewPage/AdminUserViewPage.js
@@ -1,11 +1,7 @@
 import UserCell from 'src/components/UserCell/UserCell'
 
 const AdminUserViewPage = ({ id }) => {
-  return (
-    <>
-      <UserCell id={id} />
-    </>
-  )
+  return <UserCell id={id} />
 }
 
 export default AdminUserViewPage

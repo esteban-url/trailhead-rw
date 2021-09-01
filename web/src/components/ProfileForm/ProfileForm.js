8bbe9ca9ee94467cf9756876acd18c9bcc63f969
Esteban Rojas <estebanjrojas@gmail.com>
2021-08-31 18:33:29 -0600

    using client methods to update own profile

--- web/src/components/ProfileForm/ProfileForm.js
+++ web/src/components/ProfileForm/ProfileForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { EmailField, Form, PasswordField, TextField } from '@redwoodjs/forms'
+import { EmailField, Form, TextField } from '@redwoodjs/forms'
 import FormField from 'src/components/common/FormField/FormField'
 import { navigate, routes } from '@redwoodjs/router'
 import { Button, Submit } from '../common/Button/Button'
@@ -15,13 +15,12 @@ const avatarTypes = [
   { name: 'ring' },
 ]
 
-const ProfileForm = ({ user, onSave, error, loading }) => {
-  const [manuallyResetPassword, setManuallyResetPassword] = useState(false)
+const ProfileForm = ({ user, onSave, onPasswordReset, error, loading }) => {
   const [selectedAvatarType, setSelectedAvatarType] = useState(() =>
     avatarTypes.find((x) => x.name === user?.user_metadata?.avatar_type)
   )
 
-  const { register, getValues } = useForm({
+  const { register } = useForm({
     defaultValues: user
       ? {
           name: user?.user_metadata?.full_name,
@@ -52,135 +51,74 @@ const ProfileForm = ({ user, onSave, error, loading }) => {
   const handleAvatarTypeChange = (option) => {
     setSelectedAvatarType(option)
   }
-  const handleManualPassword = () => {
-    setManuallyResetPassword(!manuallyResetPassword)
-  }
+
   const handleResetPassword = () => {
-    console.log('oi')
+    onPasswordReset()
   }
 
   return (
-    <Form
-      onSubmit={handleSubmit}
-      className="space-y-8 divide-y divide-gray-200"
-    >
-      <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-        <FormField
-          as={TextField}
-          name="name"
-          label="Name"
-          register={register}
-          validation={{ required: true }}
-        />
-        <FormField
-          as={EmailField}
-          name="email"
-          label="Email Address"
-          register={register}
-          validation={{
-            required: true,
-          }}
-        />
-        <FormField label="Avatar">
-          <AvatarTypesRadio
-            options={avatarTypes}
-            user={user}
-            defaultValue={selectedAvatarType}
-            onChange={handleAvatarTypeChange}
-          />
-        </FormField>
-        {user ? (
-          <>
-            <div className="bg-gray-50 rounded-lg sm:col-span-6">
-              <div className="px-4 py-5 sm:p-6">
-                <h3 className="leading-3 font-medium text-gray-900">
-                  Resetting the password?
-                </h3>
-                <div className="mt-2 max-w-xl text-sm text-gray-500">
-                  <p>Are you sure you want to reset the your password?</p>
-                </div>
-                <div className="mt-5">
-                  <Button onClick={handleResetPassword} variant="basic">
-                    {`${
-                      manuallyResetPassword ? "Nevermind, I don't" : 'Yes, I'
-                    } want to reset my password`}
-                  </Button>
-                  <Button onClick={handleManualPassword} variant="basic">
-                    {`${
-                      manuallyResetPassword ? "Nevermind, I don't" : 'Yes, I'
-                    } want to reset my password`}
-                  </Button>
-                  {manuallyResetPassword ? (
-                    <>
-                      <FormField
-                        as={PasswordField}
-                        name="currentPassword"
-                        label="Current password"
-                        className="sm:col-span-4 mt-4"
-                        register={register}
-                        validation={{
-                          required: 'Please enter your current password',
-                        }}
-                      />
-
-                      <FormField
-                        as={PasswordField}
-                        name="password"
-                        label="New password"
-                        className="sm:col-span-4 mt-4"
-                        register={register}
-                        validation={{
-                          required: 'Please enter your new password',
-                        }}
-                      />
-                      <FormField
-                        as={PasswordField}
-                        name="confirmPassword"
-                        label="Confirm new password"
-                        className="sm:col-span-4 mt-4"
-                        register={register}
-                        validation={{
-                          required: 'Please confirm your new password',
-                          validate: {
-                            matchesPreviousPassword: (value) => {
-                              const { password } = getValues()
-                              return (
-                                password === value || 'Passwords should match!'
-                              )
-                            },
-                          },
-                        }}
-                      />
-                    </>
-                  ) : null}
-                </div>
-              </div>
-            </div>
-          </>
-        ) : null}
-        {!user ? (
+    <>
+      <Form
+        onSubmit={handleSubmit}
+        className="space-y-8 divide-y divide-gray-200"
+      >
+        <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
           <FormField
-            as={PasswordField}
-            name="password"
-            label="Password"
+            as={TextField}
+            name="name"
+            label="Name"
             register={register}
             validation={{ required: true }}
           />
-        ) : null}
+          <FormField
+            as={EmailField}
+            name="email"
+            label="Email Address"
+            register={register}
+            validation={{
+              required: true,
+            }}
+          />
+          <FormField label="Avatar">
+            <AvatarTypesRadio
+              options={avatarTypes}
+              user={user}
+              defaultValue={selectedAvatarType}
+              onChange={handleAvatarTypeChange}
+            />
+          </FormField>
+          {user ? <></> : null}
 
-        {error ? <span>{error.message}</span> : null}
-        {loading ? <span>saving</span> : null}
-      </div>
+          {error ? <span>{error.message}</span> : null}
+          {loading ? <span>saving</span> : null}
+        </div>
 
-      <div className="pt-5">
-        <div className="flex justify-end space-x-4">
-          <Button onClick={handleCancel}>Cancel</Button>
-          <Submit variant="primary" disabled={loading}>
-            Save User
-          </Submit>
+        <div className="pt-5">
+          <div className="flex justify-end space-x-4">
+            <Button onClick={handleCancel}>Cancel</Button>
+            <Submit variant="primary" disabled={loading}>
+              Save User
+            </Submit>
+          </div>
+        </div>
+      </Form>
+
+      <div className="mt-8 bg-gray-50 rounded-lg sm:col-span-6">
+        <div className="px-4 py-5 sm:p-6">
+          <h3 className="leading-3 font-medium text-gray-900">
+            Resetting your password?
+          </h3>
+          <div className="mt-2 max-w-xl text-sm text-gray-500">
+            <p>We will send you an email with a link to reset you password</p>
+          </div>
+          <div className="mt-5">
+            <Button onClick={handleResetPassword} variant="basic">
+              Yes, I want to reset my password
+            </Button>
+          </div>
         </div>
       </div>
-    </Form>
+    </>
   )
 }
 
--- web/src/pages/SettingsPage/SettingsPage.js
+++ web/src/pages/SettingsPage/SettingsPage.js
@@ -1,41 +1,29 @@
-import { navigate, routes } from '@redwoodjs/router'
+import { Link, routes } from '@redwoodjs/router'
 import Avatar from 'boring-avatars'
 import ProfileForm from 'src/components/ProfileForm/ProfileForm'
 import { useAuth } from '@redwoodjs/auth'
 import { PageTitle } from 'src/utils/PageTitle'
 import { toast } from '@redwoodjs/web/toast'
-import { useMutation } from '@redwoodjs/web'
-
-const UPDATE_OWN_USER = gql`
-  mutation UpdateOwnUserMutation($input: UpdateUserInput!) {
-    updateOwnUser(input: $input) {
-      id
-    }
-  }
-`
-
+import { useState } from 'react'
+import { ExclamationIcon } from '@heroicons/react/outline'
 const SettingsPage = () => {
-  const { currentUser } = useAuth()
-
-  const [updateUser, { error, loading }] = useMutation(UPDATE_OWN_USER, {
-    onCompleted: () => {
-      toast.success('User successfuly updated')
-      navigate(routes.adminUsers())
-    },
-    onError: () => {
-      toast.error(`the user could not be updated.`)
-    },
-
-    // This refetches the query on the list page. Read more about other ways to
-    // update the cache over here:
-    // https://www.apollographql.com/docs/react/data/mutations/#making-all-other-cache-updates
-    // refetchQueries: [{ query: QUERY_USERS }],
-    awaitRefetchQueries: true,
-  })
-
+  const { currentUser, client } = useAuth()
+  const [done, setDone] = useState()
+  const handlePasswordReset = () => {
+    client.gotrue.requestPasswordRecovery(currentUser.email)
+  }
   const handleChange = (user) => {
-    updateUser({ variables: { id: user.id, input: user } })
-    console.log(user)
+    const updatePromise = client.gotrue
+      .currentUser()
+      .update({ email: user.email, data: user.user_metadata })
+      .then(setDone(true))
+
+    toast.promise(updatePromise, {
+      loading: <h1 className="bg-primary-600">asdasd</h1>,
+      success: 'User successfuly updated!',
+      error: 'Unable to save changes',
+    })
+    // TODO: refresh user so they dont have to log out and back in
   }
   return (
     <>
@@ -61,8 +49,60 @@ const SettingsPage = () => {
           </div>
         </div>
       </div>
-      <ProfileForm user={currentUser} onSave={handleChange} />
+      {done ? (
+        <Banner />
+      ) : (
+        <ProfileForm
+          user={currentUser}
+          onSave={handleChange}
+          onPasswordReset={handlePasswordReset}
+        />
+      )}
     </>
+  )
+}
+
+const Banner = () => {
+  return (
+    <div className=" flex flex-col justify-center text-gray-600">
+      <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">
+        <div className="flex">
+          <div className="flex-shrink-0">
+            <ExclamationIcon
+              className="h-12 w-12 text-yellow-400"
+              aria-hidden="true"
+            />
+          </div>
+          <div className="ml-3 text-sm text-yellow-700">
+            <p className="font-bold text-lg">Good news!</p>
+            <p>We&apos;ve successfuly saved the changes to your profile!</p>
+            <p className="font-bold mt-4">Not so good news:</p>{' '}
+            <p>
+              If you want to see the changes right away, you need to sign out an
+              sign back in.
+            </p>
+            <p className="mt-2">
+              We know how annpying this is, we hope to fix it soon, promise!
+            </p>
+            <p className="mt-4">
+              <Link
+                to={routes.login()}
+                className="font-medium underline text-yellow-700 hover:text-yellow-600"
+              >
+                Click here to sign out and go to the login page
+              </Link>
+              , or you can go back to{' '}
+              <Link
+                to={routes.profile()}
+                className="font-medium underline text-yellow-700 hover:text-yellow-600"
+              >
+                your profile
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
